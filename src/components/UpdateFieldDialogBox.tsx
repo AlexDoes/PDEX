e6be5a6f9c6fd20e3af9f334fa060f832d78a879
AlexDoes <alexsw.191@gmail.com>
2023-05-31 19:19:55 -0400

    profile button

--- src/components/UpdateFieldDialogBox.tsx
+++ src/components/UpdateFieldDialogBox.tsx
@@ -65,7 +65,7 @@ export default function UpdateDataComponent({
   return (
     <div className="mt-2">
       <button
-        className="  w-[185px] text-[#ffffffbf] border-green-500 rounded-md px-1 sm:px-5 py-1 shadow-lg bg-[#9bb59b]  hover:bg-[#c1e1c183]  font-semibold"
+        className="  w-[190px] text-[#ffffffbf] border-green-500 rounded-md px-1 sm:px-5 py-1 shadow-lg bg-[#9bb59b]  hover:bg-[#c1e1c183]  font-semibold"
         onClick={handleOpen}
       >
         Update {map[field]}
