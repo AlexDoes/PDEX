055a105a8c728ca6c20f58215ac227c4985e894f
AlexDoes <alexsw.191@gmail.com>
2023-06-20 00:59:43 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,7 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* <ScreenChecker /> */}
         <Footer />
       </SessionProvider>
-      {/* <Analytics /> */}
+      <Analytics />
     </>
   );
 }
